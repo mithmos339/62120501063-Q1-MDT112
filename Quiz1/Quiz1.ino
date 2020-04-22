321f97f7fad15dcbe634afa587478da19fe5eeae
monmos339@gmail.com <monmos339@gmail.com>
2020-04-22 10:40:53 +0700

    ERROR

--- Quiz1/Quiz1.ino
+++ Quiz1/Quiz1.ino
@@ -1,53 +1,69 @@
-void setup() {
-    pinMode(13, OUTPUT);
-    pinMode(12, OUTPUT);
-    pinMode(11, OUTPUT);
-    pinMode(10, OUTPUT);
-    pinMode(9, OUTPUT);
-    pinMode(8, OUTPUT);
-    pinMode(7, OUTPUT);
-    pinMode(6, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(2, OUTPUT);
+void setup(){
+    Serial.begin(9600);
+    pinMode(2,INPUT_PULLUP);   //LED Pin 3,4,5,6,7,9,10,11,12,13
+    pinMode(3,OUTPUT);
+    pinMode(4,OUTPUT);
+    pinMode(5,OUTPUT);
+    pinMode(6,OUTPUT);
+    pinMode(7,OUTPUT);
+    pinMode(8,OUTPUT);
+    pinMode(9,OUTPUT);
+    pinMode(10,OUTPUT);
+    pinMode(11,OUTPUT);
+    pinMode(12,OUTPUT);
+    pinMode(13,OUTPUT);    
+    tone(8,200,500);
+    delay(250);
+    tone(8,400,100);
 }
 
-void loop() {
-    digitalWrite(2, 1);
-    delay(100);
-    digitalWrite(2, 0);    
-    digitalWrite(3, 1);
-    delay(100);
-    digitalWrite(3, 0);
-    digitalWrite(4, 1);
-    delay(100);
-    digitalWrite(4, 0);
-    digitalWrite(5, 1);
-    delay(100);
-    digitalWrite(5, 0);
-    digitalWrite(6, 1);
-    delay(100);
-    digitalWrite(6, 0);
-    digitalWrite(7, 1);
-    delay(100);
-    digitalWrite(7, 0);
-    digitalWrite(8, 1);
-    delay(100);
-    digitalWrite(8, 0);
-    digitalWrite(9, 1);
-    delay(100);
-    digitalWrite(9, 0);
-    digitalWrite(10, 1);
-    delay(100);
-    digitalWrite(10, 0);
-    digitalWrite(11, 1);
-    delay(100);
-    digitalWrite(11, 0);
-    digitalWrite(12, 1);
-    delay(100);
-    digitalWrite(12, 0);
-    digitalWrite(13, 1);
-    delay(100);
-    digitalWrite(13, 0);
-}
+int c = 0;  //count
+int l = 3;  //light
+int t = 100;  //time
+
+void loop(){
+    for (l = 3;l <= 13;l++)
+    {
+        if(l == 8)
+        {
+            digitalWrite(l,0);
+            continue;
+        }
+        digitalWrite(l,1);
+        while (digitalRead(2)==0)
+        {   
+            delay(200);
+            if (digitalRead(2)==1)
+            {               
+                c++;
+                tone(8,400,100);        
+                Serial.println("Faster X 2 (Current Speed : X" + String(pow(2,c)) + ")");  //Current Speed : X(2^n)                              
+                t = t/2;
+            }
+        }     
+        delay(t);
+        digitalWrite(l,0);               
+    }
+    for (l = 13 ; l >= 3 ; l--)
+    {
+        if(l == 8)
+        {
+            digitalWrite(l,0);
+            continue;
+        }
+        digitalWrite(l, 1);
+        while (digitalRead(2)==0)
+        {   
+            delay(200);
+            if (digitalRead(2)==1)
+            {               
+                c++;
+                tone(8,400,100);        
+                Serial.println("Faster X 2 (Current Speed : X" + String(pow(2, c)) + ")");   //Current Speed : X(2^n)                             
+                t = t/2;
+            }
+        }     
+        delay(t);
+        digitalWrite(l, 0);         
+    }
+}    
